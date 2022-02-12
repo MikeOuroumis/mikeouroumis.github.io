d894343e51038881548440214a333eab04d5f759
Mike <michalisouroumis@gmail.com>
2022-02-12 12:42:35 +0200

    text animation

--- app.js
+++ app.js
@@ -8,3 +8,45 @@ const mobileMenu = () =>{
 }
 
 menu.addEventListener('click', mobileMenu);
+
+//text animation
+const text = document.querySelector(".hero_text");
+const strText = text.textContent;
+const splitText = strText.split("");
+
+text.textContent = "";
+
+for(let letter of splitText){
+    text.innerHTML += "<span>" + letter + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick(){
+    const span = text.querySelectorAll("span")[char];
+    span.classList.add("fade");
+/*
+    if(span.innerHTML != " "){
+    }
+    else{
+        span.classList.add("empty_space")
+    }*/
+    char++;
+
+
+
+    if (char === splitText.length){
+        complete();
+        return;
+    }
+
+
+}
+
+
+
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
