f6787371786e6de531194de0f0d31aa1e3364d9a
Mike <michalisouroumis@gmail.com>
2022-02-13 19:57:47 +0200

    skills animation bars

--- app.js
+++ app.js
@@ -50,3 +50,57 @@ function complete(){
     clearInterval(timer);
     timer = null;
 }
+
+const aboutSection = document.querySelector(".skills-bars-container");
+
+const options = {
+    root: null,
+    threshold: 0.25,
+    rootMargin: "-150px"
+};
+//skills
+document.querySelector('.bar .progress-line.html').style.display = 'none';
+document.querySelector('.bar .progress-line.css').style.display = 'none';
+document.querySelector('.bar .progress-line.jquery').style.display = 'none';
+document.querySelector('.bar .progress-line.ReactJS').style.display = 'none';
+document.querySelector('.progress-line.Wordpress').style.display = 'none';
+document.querySelector('.bar .progress-line.python').style.display = 'none';
+
+
+//soft skills
+document.querySelector('.bar .progress-line-soft-skills').style.display = 'none';
+document.querySelector('.bar .progress-line-soft-skills.creativity').style.display = 'none';
+document.querySelector('.bar .progress-line-soft-skills.adaptivity').style.display = 'none';
+document.querySelector('.bar .progress-line-soft-skills.problem-solving').style.display = 'none';
+document.querySelector('.bar .progress-line-soft-skills.accountability').style.display = 'none';
+document.querySelector('.bar .progress-line-soft-skills.empathy').style.display = 'none';
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+
+            //skills
+            document.querySelector('.bar .progress-line.html').style.display = 'grid';
+            document.querySelector('.bar .progress-line.css').style.display = 'grid';
+            document.querySelector('.bar .progress-line.jquery').style.display = 'grid';
+            document.querySelector('.bar .progress-line.ReactJS').style.display = 'grid';
+            document.querySelector('.progress-line.Wordpress').style.display = 'grid';
+            document.querySelector('.bar .progress-line.python').style.display = 'grid';
+
+
+
+            //soft skills
+            document.querySelector('.bar .progress-line-soft-skills').style.display = 'grid';
+            document.querySelector('.bar .progress-line-soft-skills.creativity').style.display = 'grid';
+            document.querySelector('.bar .progress-line-soft-skills.adaptivity').style.display = 'grid';
+            document.querySelector('.bar .progress-line-soft-skills.problem-solving').style.display = 'grid';
+            document.querySelector('.bar .progress-line-soft-skills.accountability').style.display = 'grid';
+            document.querySelector('.bar .progress-line-soft-skills.empathy').style.display = 'grid';
+
+            
+            console.log(entry);
+        }
+    });
+}, options);
+
+observer.observe(aboutSection);
